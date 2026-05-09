2cf42ccc21ee80c57036246fe3230a63de33d778
mistvle <mistle.roblox@gmail.com>
2026-05-09 07:30:42 -0700

    first commit

---
+++ events/ready.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: "ready",
+  once: true,
+
+  async execute(client) {
+    console.log(`✅ Logged in as ${client.user.tag}`);
+
+    client.user.setActivity("Ocean Customs", {
+      type: 3 // WATCHING
+    });
+  }
+};
