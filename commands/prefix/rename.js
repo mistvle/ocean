170b9782079a86839c6a219f658d7d068e2f98d4
mistvle <mistle.roblox@gmail.com>
2026-05-10 22:15:46 -0700

    A

---
+++ commands/prefix/rename.js
@@ -0,0 +1,16 @@
+module.exports = {
+    customId: "rename",
+    async execute (message, args) {
+        const REQUIRED_ROLE_ID = "1502473790498672741";
+        const hasRole = message.member.roles.cache.has(REQUIRED_ROLE_ID);
+        const isAdmin = message.permisions.has("Administrator");
+        if (!isAdmin && !hasRole) {
+            return message.reply("<:xMark:1502681150231941282>  You do not have permission to run this command.")
+
+        }
+
+        const name = args.slice(0).join(" ");
+        await message.channel.rename(name);
+        await message.reply(`<:check:1502681323989504000> Successfully renamed ticket to **${name}.**`)
+    }
+}
