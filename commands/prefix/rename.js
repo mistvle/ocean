0183dc8e6a37a272c0ffb6a823e4d11390b8b76e
mistvle <mistle.roblox@gmail.com>
2026-05-10 22:21:47 -0700

    A

--- commands/prefix/rename.js
+++ commands/prefix/rename.js
@@ -1,8 +1,7 @@
 module.exports = {
     name: "rename",
     async execute (message, args) {
-        const REQUIRED_ROLE_ID = "1502473790498672741";
-        const hasRole = message.member.roles.cache.has(REQUIRED_ROLE_ID);
+        const hasRole = message.member.roles.cache.has("1502473790498672741");
         const isAdmin = message.member.permissions.has("Administrator");
         if (!isAdmin && !hasRole) {
             return message.reply("<:xMark:1502681150231941282> You do not have permission to run this command.")
