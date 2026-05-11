1d5f8e8501dc134db32ea618863cc1f0612cac5d
mistvle <mistle.roblox@gmail.com>
2026-05-10 22:24:08 -0700

    A

--- commands/prefix/rename.js
+++ commands/prefix/rename.js
@@ -1,18 +1,55 @@
 module.exports = {
-    name: "rename",
-    async execute (message, args) {
-        const hasRole = message.member.roles.cache.has("1502473790498672741");
-        const isAdmin = message.member.permissions.has("Administrator");
-        if (!isAdmin && !hasRole) {
-            return message.reply("<:xMark:1502681150231941282> You do not have permission to run this command.")
+  name: "rename",
 
-        }
+  async execute(message, args) {
+    if (message.author.bot) return;
 
-        const name = args.slice(0).join(" ");
-        if (!name) {
-            return message.reply("<:xMark:1502681150231941282> You must provide a valid name.")
-        }
-        await message.channel.setName(name);
-        await message.reply(`<:check:1502681323989504000> Successfully renamed ticket to **${name}.**`)
+    const STAFF_ROLE_IDS = "1502473790498672741";
+
+    const isAdmin = message.member.permissions.has("Administrator");
+    const hasRole = message.member.roles.cache.has(STAFF_ROLE_IDS);
+
+    if (!isAdmin && !hasRole) {
+      return message.reply({
+        content: "<:xMark:1502681150231941282> You do not have permission to use this command.",
+        allowedMentions: { repliedUser: false }
+      });
     }
-}
+
+    if (!message.channel.topic) {
+      return message.reply({
+        content: "<:xMark:1502681150231941282> This channel is not a ticket channel.",
+        allowedMentions: { repliedUser: false }
+      });
+    }
+
+    if (!/^\d+$/.test(message.channel.topic)) {
+      return message.reply({
+        content: "<:xMark:1502681150231941282> You can only rename ticket channels.",
+        allowedMentions: { repliedUser: false }
+      });
+    }
+
+    const newName = args.slice(0).join(" ");
+
+    if (!newName) {
+      return message.reply({
+        content: "<:xMark:1502681150231941282> Failed to detect a valid new ticket name.",
+        allowedMentions: { repliedUser: false }
+      });
+    }
+
+    try {
+      await message.channel.setName(newName);
+
+      await message.reply(`<:check:1502681323989504000> Successfully renamed ticket to ${newName}.`);
+    } catch (err) {
+      console.error(err);
+
+      return message.reply({
+        content: "<:xMark:1497479677755785379> An eror occured.",
+        allowedMentions: { repliedUser: false }
+      });
+    }
+  }
+};
