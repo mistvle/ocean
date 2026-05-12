889eb0039543d24aa2510d91fc003a5de22376ff
mistvle <mistle.roblox@gmail.com>
2026-05-11 23:10:20 -0700

    A

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,44 @@
+module.exports = {
+  name: "guildMemberAdd",
+
+  async execute(member, client) {
+
+    const channel = member.guild.channels.cache.get(process.env.WELCOME_CHANNEL_ID);
+
+    if (channel) {
+      channel.send({
+  "content": "<:wave:1503639751465631745> Welcome ${member} to **Ocean Customs** — your go-to ER:LC service hub provider. If you're looking to order top-notch products for the cheapest prices, order today in our [services](https://discord.com/channels/1502473705127809044/1502479201326858461) channel.",
+  "components": [
+    {
+      "type": 1,
+      "components": [
+        {
+          "style": 2,
+          "type": 2,
+          "label": "${member.guild.memberCount}",
+          "emoji": {
+            "id": "1502684333209817149",
+            "name": "m_Heart",
+            "animated": false
+          },
+          "disabled": true,
+          "flow": {
+            "actions": []
+          },
+          "custom_id": "p_301237153558433793"
+        },
+        {
+          "type": 2,
+          "style": 5,
+          "label": "Dashboard",
+          "url": "https://discord.com/channels/1502473705127809044/1502478453050572921",
+        }
+      ]
+    }
+  ]
+});
+    }
+
+
+  }
+};
